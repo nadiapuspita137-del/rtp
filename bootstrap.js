049fba4bf33187df39ed43807a3bb24693dbfb59
nadiapuspita137-del <nadiapuspita137@gmail.com>
2026-08-12 17:27:07 +0700

    chore: remove unused bootstrap loader

--- bootstrap.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  "use strict";
-  fetch("/api/data", { cache: "no-store" })
-    .then(response => { if (!response.ok) throw new Error("API unavailable"); return response.json(); })
-    .then(data => { window.RTP_DATA = data; window.dispatchEvent(new CustomEvent("rtp:data-ready")); })
-    .catch(() => { window.dispatchEvent(new CustomEvent("rtp:data-ready")); });
-})();
